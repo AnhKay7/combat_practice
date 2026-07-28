af042a576d4c322957e709452806fd9481f03633
Anh Kiet <anhskay69@gmail.com>
2026-07-28 21:58:27 +0700

    save

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,36 +4,27 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float move_input;
-    //[Header("Movement Stats")]
-    public float move_speed = 8f;
+    [SerializeField] private float x_input;
+    [SerializeField] private float move_speed = 8f;
+    [SerializeField] private float jump_force = 8f;
     private PlayerControls controls;
 
-    private void Awake()
-    {
-        controls = new PlayerControls();
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        move_input = 0f;
-        if (Keyboard.current != null)
-        {
-            if (Keyboard.current.leftArrowKey.isPressed)
-                move_input = -1f;
-            if (Keyboard.current.rightArrowKey.isPressed)
-                move_input = 1f;
-        }
-        //move_input = controls.Gameplay.Move.ReadValue<float>();
+        x_input = 0f;
+        x_input = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            rb.linearVelocity = new Vector2(rb.linearVelocityX, jump_force);
+
     }
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(move_input * move_speed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(x_input * move_speed, rb.linearVelocity.y);
     }
 }
